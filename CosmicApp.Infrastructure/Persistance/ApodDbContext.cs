092fa5c1d6e3609aa83fce071913ccbb7b1d953c
Grzegorz Haruk <g.haruk@godeltech.com>
2024-06-30 12:54:18 +0200

    Update architecture, add EFCore

---
+++ CosmicApp.Infrastructure/Persistance/ApodDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CosmicApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmicApp.Infrastructure.Persistance
+{
+    internal class ApodDbContext : DbContext
+    {
+        internal DbSet<Apod> Apods { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CosmicDb;Trusted_Connection=True;");
+        }
+    }
+}
