b14dcf2c42011abb60e371943b2467ad99998ea1
Grzegorz Haruk <g.haruk@godeltech.com>
2024-07-14 15:31:59 +0200

    Add tests, refactor

---
+++ CosmicApp.Application.Tests/Queries/GetNasaApodByDate/GetNasaApodByDateQueryValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using CosmicApp.Application.Queries.GetNasaApodByDate;
+using FluentValidation.TestHelper;
+
+namespace CosmicApp.Application.Tests.Queries.GetNasaApodByDate
+{
+    public class GetNasaApodByDateQueryValidatorTests
+    {
+        [Theory, MemberData(nameof(Cases))]
+        public void Validator_ValidQuery_ShouldNotHaveValidationErrors(DateTime date)
+        {
+            // arrange
+            var query = new GetNasaApodByDateQuery
+            {
+                Date = date
+            };
+
+            var validator = new GetNasaApodByDateQueryValidator();
+
+            // act
+
+            var result = validator.TestValidate(query);
+
+            // assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validator_InvalidQuery_ShouldHaveValidationErrors()
+        {
+            // arrange
+            var query = new GetNasaApodByDateQuery() { Date = DateTime.UtcNow.AddDays(1) };
+
+            var validator = new GetNasaApodByDateQueryValidator();
+
+            // act
+
+            var result = validator.TestValidate(query);
+
+            // assert
+
+            result.ShouldHaveAnyValidationError();
+        }
+
+        public static TheoryData<DateTime> Cases = new()
+        {
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddDays(-1),
+        };
+    }
+}
--- CosmicApp/Controllers/ApodController.cs
+++ CosmicApp/Controllers/ApodController.cs
@@ -12,9 +12,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace CosmicApp.Api.Controllers
 {
     [ApiController]
-    [Route("api/apods")]    
+    [Route("api/apods")]
+    [Authorize(Roles = UserRoles.User)]
     public class ApodController : ControllerBase
-    {        
+    {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
 
@@ -24,34 +25,32 @@ namespace CosmicApp.Api.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
-        [Route("date")]
+        [HttpGet("date")]
         public async Task<IActionResult> GetNasaApodAsync(DateTime date)
         {
             var apod = await _mediator.Send(new GetNasaApodByDateQuery { Date = date });
             return Ok(apod);
         }
 
-        [HttpGet]
-        [Route("id")]        
-        public async Task<IActionResult> GetApodByIdAsync(int id)
+        [HttpGet("id")]
+        public async Task<IActionResult> GetApodByIdAsync([FromQuery] int id)
         {
             var apod = await _mediator.Send(new GetApodByIdQuery() { Id = id });
             return Ok(apod);
         }
 
-        [HttpGet("all")]        
+        [HttpGet("all")]
         public async Task<IActionResult> GetAllApods()
         {
             var result = await _mediator.Send(new GetAllApodsQuery());
             return Ok(result);
-        }        
+        }
 
         [HttpPost]
         public async Task<IActionResult> CreateApod([FromBody] ApodDto apodDto)
         {
             int id = await _mediator.Send(_mapper.Map<CreateApodCommand>(apodDto));
-            
+
             return Ok(id);
         }
     }
