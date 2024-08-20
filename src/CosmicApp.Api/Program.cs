655f234b6edf7383d808e6f2c12a21444a668350
Grzegorz Haruk <g.haruk@godeltech.com>
2024-08-20 18:20:49 +0200

    Add logger

--- src/CosmicApp.Api/Program.cs
+++ src/CosmicApp.Api/Program.cs
@@ -5,50 +5,71 @@ using CosmicApp.Domain.Entities;
 using CosmicApp.Infrastructure.Extensions;
 using CosmicApp.Infrastructure.Seeders;
 using Microsoft.Extensions.Options;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddCors();
-
-builder.Services.Configure<NasaApiOptions>(builder.Configuration.GetSection("NasaApiOptions"));
-
-builder.Services.AddHttpClient("ApodService", (options, client) =>
+try
 {
-    var apiKey = options.GetRequiredService<IOptions<NasaApiOptions>>().Value;
 
-    client.BaseAddress = new Uri($"https://api.nasa.gov/planetary/apod?api_key={apiKey.NasaApiKey}&");
-});
 
-builder.AddPresentation();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    // Add services to the container.
 
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
-await seeder.Seed();
+    builder.Services.AddCors();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+    builder.Services.Configure<NasaApiOptions>(builder.Configuration.GetSection("NasaApiOptions"));
+
+    builder.Services.AddHttpClient("ApodService", (options, client) =>
+    {
+        var apiKey = options.GetRequiredService<IOptions<NasaApiOptions>>().Value;
+
+        client.BaseAddress = new Uri($"https://api.nasa.gov/planetary/apod?api_key={apiKey.NasaApiKey}&");
+    });
+
+    builder.AddPresentation();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddApplication();
+
+    builder.Host.UseSerilog((context, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration));
+
+    var app = builder.Build();
+
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
+    await seeder.Seed();
+
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(options => options.WithOrigins().AllowAnyOrigin());
+
+    app.UseHttpsRedirection();
+
+    app.MapGroup("api/identity").MapIdentityApi<User>();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
+catch (Exception e)
+{
+    Log.Fatal(e, "App exploded on start");
+}
 
-app.UseCors(options => options.WithOrigins().AllowAnyOrigin());
-
-app.UseHttpsRedirection();
-
-app.MapGroup("api/identity").MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
